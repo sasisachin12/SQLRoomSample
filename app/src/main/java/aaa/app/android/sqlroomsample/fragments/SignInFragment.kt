6ca8752f2290a8a9dd881169d201322f1b017b10
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-09-19 16:35:26 +0100

    Created sign in fragment with layout

---
+++ app/src/main/java/aaa/app/android/sqlroomsample/fragments/SignInFragment.kt
@@ -0,0 +1,22 @@
+package aaa.app.android.sqlroomsample.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import aaa.app.android.sqlroomsample.R
+
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+
+}
