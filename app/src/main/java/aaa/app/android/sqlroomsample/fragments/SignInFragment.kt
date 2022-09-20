a413ca9fb19392fbd92b6434db69b067fd659cbd
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-09-20 10:26:02 +0100

    Added view binding

--- app/src/main/java/aaa/app/android/sqlroomsample/fragments/SignInFragment.kt
+++ app/src/main/java/aaa/app/android/sqlroomsample/fragments/SignInFragment.kt
@@ -6,16 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import aaa.app.android.sqlroomsample.R
+import aaa.app.android.sqlroomsample.databinding.FragmentSignInBinding
 
 
 class SignInFragment : Fragment() {
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
