ee992527c899ac52b2e2ecfbd87b6ffe35fd3b6e
Sasikumar <sasisachin12@yahoo.in>
2024-07-22 13:12:45 +0530

    updated  jetpack compose ui

---
+++ app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/MainActivity.kt
@@ -0,0 +1,26 @@
+package aaa.app.android.sqlroomsample.jetpack.screen
+
+import aaa.app.android.sqlroomsample.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                ContextCompat.getColor(this, R.color.immersive_sys_ui)
+            )
+        )
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            OwlApp { finish() }
+        }
+    }
+}
---
+++ app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/OwlApp.kt
@@ -0,0 +1,87 @@
+package aaa.app.android.sqlroomsample.jetpack.screen
+
+import aaa.app.android.sqlroomsample.jetpack.screen.courses.CourseTabs
+import aaa.app.android.sqlroomsample.jetpack.screen.theme.BlueTheme
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.primarySurface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import java.util.Locale
+
+@Composable
+fun OwlApp(finishActivity: () -> Unit) {
+    BlueTheme {
+        val tabs = remember { CourseTabs.values() }
+        val navController = rememberNavController()
+        Scaffold(
+            backgroundColor = MaterialTheme.colors.primarySurface,
+            bottomBar = { OwlBottomBar(navController = navController, tabs) }
+        ) { innerPaddingModifier ->
+            NavGraph(
+                finishActivity = finishActivity,
+                navController = navController,
+                modifier = Modifier.padding(innerPaddingModifier)
+            )
+        }
+    }
+}
+
+@Composable
+fun OwlBottomBar(navController: NavController, tabs: Array<CourseTabs>) {
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+        ?: CourseTabs.FEATURED.route
+
+    val routes = remember { CourseTabs.values().map { it.route } }
+    if (currentRoute in routes) {
+        BottomNavigation(
+            Modifier.windowInsetsBottomHeight(
+                WindowInsets.navigationBars.add(WindowInsets(bottom = 56.dp))
+            )
+        ) {
+            tabs.forEach { tab ->
+                BottomNavigationItem(
+                    icon = { Icon(painterResource(tab.icon), contentDescription = null) },
+                    label = { Text(stringResource(tab.title).uppercase(Locale.getDefault())) },
+                    selected = currentRoute == tab.route,
+                    onClick = {
+                        if (tab.route != currentRoute) {
+                            navController.navigate(tab.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    },
+                    alwaysShowLabel = false,
+                    selectedContentColor = MaterialTheme.colors.secondary,
+                    unselectedContentColor = LocalContentColor.current,
+                    modifier = Modifier.navigationBarsPadding()
+                )
+            }
+        }
+    }
+}
