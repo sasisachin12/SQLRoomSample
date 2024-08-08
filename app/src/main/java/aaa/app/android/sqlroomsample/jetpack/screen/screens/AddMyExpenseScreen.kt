ee7a368c926496e8c6329d7980532bd5745e1719
Sasikumar <sasisachin12@yahoo.in>
2024-08-08 23:12:37 +0530

    added time picker

--- app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/screens/AddMyExpenseScreen.kt
+++ app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/screens/AddMyExpenseScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
@@ -24,7 +25,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,8 +41,10 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import java.util.Calendar
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddMyExpenseScreen(
     modifier: Modifier = Modifier,
@@ -60,6 +66,15 @@ fun AddMyExpenseScreen(
             ExpenseAmountFiled()
             // DatePickerDocked()
             DatePickerModal { System.currentTimeMillis() }
+            val currentTime = Calendar.getInstance()
+
+            DialWithDialogExample({
+                TimePickerState(
+                    initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+                    initialMinute = currentTime.get(Calendar.MINUTE),
+                    is24Hour = true,
+                )
+            })
             SaveExpenseButton(onClick = viewModel::createNewTask)
         }
 
@@ -146,6 +161,8 @@ fun DatePickerModal(
             .padding(12.dp)
             .fillMaxWidth()
     )
+
+
     if (showDatePicker) {
         DatePickerDialog(
             onDismissRequest = { showDatePicker = false },
@@ -168,6 +185,73 @@ fun DatePickerModal(
     }
 
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DialWithDialogExample(
+    onConfirm: (TimePickerState) -> Unit,
+
+    ) {
+    val currentTime = Calendar.getInstance()
+    var showTime by remember {
+        mutableStateOf(false)
+    }
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+    OutlinedTextField(
+        value = timePickerState.hour.toString() + " : " + timePickerState.minute.toString(),
+        onValueChange = { },
+        label = { Text(text = "Time") },
+        readOnly = true,
+        trailingIcon = {
+            IconButton(onClick = { showTime = !showTime }) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Select date"
+                )
+            }
+        },
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+    )
+    if (showTime)
+        TimePickerDialog(
+            onDismiss = { showTime = false },
+            onConfirm = { showTime = false }
+        ) {
+            TimePicker(
+                state = timePickerState,
+            )
+        }
+}
+
+@Composable
+fun TimePickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Dismiss")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        text = { content() }
+    )
+}
+
 
 
 
