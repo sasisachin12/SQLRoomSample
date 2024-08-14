82e4c3a41f399747b4da0c354fb47fdea68d6b92
Sasikumar <sasisachin12@yahoo.in>
2024-08-15 00:11:07 +0530

    expense list ui  fetch functions added

--- app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/screens/ExpenseListScreen.kt
+++ app/src/main/java/aaa/app/android/sqlroomsample/jetpack/screen/screens/ExpenseListScreen.kt
@@ -1,36 +1,40 @@
 package aaa.app.android.sqlroomsample.jetpack.screen.screens
 
 import aaa.app.android.sqlroomsample.entity.ExpenseInfo
+import aaa.app.android.sqlroomsample.util.Utils.convertLongToTime
 import aaa.app.android.sqlroomsample.viewmodel.ExpenseViewModel
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 
 @Composable
 fun ExpenseListScreen() {
 
-    LazyColumn(
+    Column(
         modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .statusBarsPadding()
+            .fillMaxSize()
+            .background(color = Color.White)
     ) {
-
-        item {
-            ExpenseAppBar()
-        }
-        item {
-            ExpenseList()
-        }
-
-
+        ExpenseAppBar()
+        ExpenseList()
     }
+
+
 }
 
 @Composable
@@ -38,19 +42,42 @@ fun ExpenseList(viewModel: ExpenseViewModel = hiltViewModel()) {
     LaunchedEffect(true) {
         viewModel.getAllExpense()
     }
-    val list = viewModel.expenseList.value as List<ExpenseInfo>
-    LazyColumn() {
+    val list = viewModel.expenseList.collectAsState().value
+    list?.let {
+        LazyColumn {
 
-        items(items = list,
-            itemContent = { ExpenseItemRow(it) })
+            items(items = list,
+                itemContent = {
+                    ExpenseItemRow(it)
+                })
 
+
+        }
     }
+
 
 }
 
 @Composable
-fun ExpenseItemRow(it: ExpenseInfo) {
 
+fun ExpenseItemRow(it: ExpenseInfo) {
+    val modifier = Modifier
+        .padding(2.dp)
+        .padding(2.dp)
+
+
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        Text(text = convertLongToTime(it.date), modifier = modifier.weight(1f), color = Color.Gray)
+        Text(text = it.expense, modifier = modifier.weight(0.5f), color = Color.Black)
+        Text(
+            text = it.amount,
+            modifier = modifier
+                .weight(0.5f)
+                .fillMaxWidth(),
+            color = Color.Magenta
+        )
+
+    }
 
 }
 
